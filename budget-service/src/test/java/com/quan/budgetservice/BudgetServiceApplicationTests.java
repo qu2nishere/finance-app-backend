94b81b1564fc48ec5d7c2c4d47a039eb0a66aec6
qu2nishere <nghiemquan200@gmail.com>
2026-08-25 17:36:48 +0700

    init: initial commit

---
+++ budget-service/src/test/java/com/quan/budgetservice/BudgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quan.budgetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
